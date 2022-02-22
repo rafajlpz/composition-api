7862243324ca7ce0a2749e31321f9da5d5598d0c
rafajlpz <frilo_94@hotmail.com>
2022-02-22 20:31:00 +0100

    fetch y onMounted
    -Uso de sugar setup
    -Uso de onMounted en vez de fetch

---
+++ src/views/Paises.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Lista de Paises</h1>
+  <p v-for="(pais, index) in arrayData" :key="index">{{ pais.name }}</p>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+
+const arrayData = ref([]);
+
+onMounted(async () => {
+  try {
+        const res = await fetch("api.json");
+        arrayData.value = await res.json();
+  } catch (error) {
+        console.log(error);
+  }
+});
+</script>
